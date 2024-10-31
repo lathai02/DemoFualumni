22e856762703f50b63dc07011bb23000d9b74822
Thang Bui <thangbdhe170642@fpt.edu.vn>
2024-11-01 02:02:11 +0700

    add authen

---
+++ ApiAngular/Controllers/AccountController.cs
@@ -0,0 +1,116 @@
+﻿using ApiAngular.Models;
+using ApiAngular.Models.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Fualumni_demoContext _context;
+        private readonly HttpClient _httpClient;
+
+        public AccountController(IConfiguration configuration, Fualumni_demoContext context, HttpClient httpClient)
+        {
+            _configuration = configuration;
+            _context = context;
+            _httpClient = httpClient;
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginRequestDTO request)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserName == request.Email && u.Password == request.Password);
+            if (user == null)
+            {
+                return Unauthorized("Invalid login attempt.");
+            }
+            var externalApiUrl = "http://fal-dev.eba-55qpmvbp.ap-southeast-1.elasticbeanstalk.com/api/auth/login"; // Replace with actual URL
+
+            var content = new StringContent(JsonConvert.SerializeObject(new
+            {
+                username = "khanm",
+                password = "123456"
+            }), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(externalApiUrl, content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return Unauthorized("Invalid login attempt.");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<ExternalLoginResponse>(responseContent);
+
+            var token = GenerateJwtToken(user);
+            return Ok(new { SystemToken = responseObject.Token,UserToken = token });
+        }
+        public class ExternalLoginResponse
+        {
+            public string Token { get; set; }
+            public int UserRole { get; set; }
+            public string SystemName { get; set; }
+        }
+
+        //[HttpPost("Register")]
+        //public IActionResult Register(RegisterRequestDTO request)
+        //{
+        //    try
+        //    {
+        //        var user = _userService.RegisterUser(request);
+        //        var token = GenerateJwtToken(user);
+        //        return Ok(new { Token = token });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Conflict();
+        //    }
+        //}
+
+        //[HttpPost("ChangePassword")]
+        //public IActionResult ChangePassword(ChangePasswordRequestDTO request)
+        //{
+        //    var result = _userService.ChangePassword(request);
+        //    if (!result)
+        //    {
+        //        return BadRequest("Password change failed.");
+        //    }
+        //    return Ok("Password changed successfully.");
+        //}
+
+        private string GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.NameIdentifier, user.StudentNumber.ToString()),
+            new Claim("StudentNumber", user.StudentNumber.ToString())
+        };
+
+            var role = user.Role == 1 ? "Admin" : (user.Role == 2 ? "Staff" : "User");
+
+            claims.Add(new Claim(ClaimTypes.Role, role));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- ApiAngular/Program.cs
+++ ApiAngular/Program.cs
@@ -1,11 +1,66 @@
 ﻿using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.S3;
+using ApiAngular.Models.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    // Define the Bearer token security scheme
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter JWT token in the format 'Bearer {token}'",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    // Apply the Bearer token security scheme to all endpoints
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
@@ -16,7 +71,8 @@ builder.Services.AddSingleton<IDynamoDBContext, DynamoDBContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<Fualumni_demoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabase")));
 var app = builder.Build();
 
 app.UseCors(builder =>
